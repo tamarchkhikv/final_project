ba540397dbee972f710d7b273ddb1f597d501e89
tamarchkhik <tamarichkhikvadze1@gmail.com>
2024-09-22 15:54:10 +0400

    Install react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+
+      <Route path="/" element={<Home />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+
+    </Routes>
+
   );
 }
 
