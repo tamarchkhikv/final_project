86caac21d6477113ab4e7c4a13ce7caf30e49b42
tamarchkhik <tamarichkhikvadze1@gmail.com>
2024-09-23 13:39:15 +0400

    Install Redux Toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,24 @@ import './App.css';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
+import Counter from './pages/Counter';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <Routes>
+    <Provider store={store}>
+      <Routes>
 
-      <Route path="/" element={<Home />} />
-      <Route path="/Contact" element={<Contact />} />
-      <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/counter" element={<Counter />} />
 
-    </Routes>
+
+      </Routes>
+
+    </Provider>
 
   );
 }
---
+++ src/store/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counter from './app/counter';
+
+
+export const store = configureStore({
+  reducer: {
+    counter: counter,
+    
+  },
+})
+
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
