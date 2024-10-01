799ff974e931aa1e284176762607c0b9bdd1d287
tamarchkhik <tamarichkhikvadze1@gmail.com>
2024-10-01 17:04:00 +0400

    add reset password page

---
+++ src/components/ResetPasswordSectionTwo.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const ResetPasswordSectionTwo= () => {
+    return (
+        <div className="flex justify-center mt-32 mb-[148px]">
+            <div className="w-[320px]">
+               
+                <div className="flex flex-col">
+                    <label>New password</label>
+                    <input className="w-[320px] h-[45px] rounded-md border-[1px] px-[15px] py-[10px] border-[#E6E7E8]"></input>
+                </div>
+                <div className="flex flex-col mt-4">
+                    <label>Confirm password</label>
+                    <input className="w-[320px] h-[45px] rounded-md border-[1px] px-[15px] py-[10px] border-[#E6E7E8]"></input>
+                </div>
+
+                
+
+                <button className="w-[318px] h-[44px] py-3 bg-[#0E1422] text-white rounded-[4px] mt-10 text-sm">Reset password</button>
+
+               
+
+            </div>
+
+        </div>
+
+    )
+}
+export default ResetPasswordSectionTwo;
--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import DiscountBanner from "../components/DiscountBanner";
 import Header from "../components/Header";
+import ResetPasswordSectionOne from "../components/ResetPasswordSectionOne";
+import ResetPasswordSectionTwo from "../components/ResetPasswordSectionTwo";
+import FooterBlack from "../components/FooterBlack";
 
 const ResetPassword =()=>{
     return(
         <div>
             <DiscountBanner/>
             <Header/>
+            <ResetPasswordSectionOne/>
+            <ResetPasswordSectionTwo/>
+            <FooterBlack/>
 
         </div>
     )
