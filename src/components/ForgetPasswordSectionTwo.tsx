ad33e016c4371281095ae12d41bb9ab183a231d0
tamarchkhik <tamarichkhikvadze1@gmail.com>
2024-10-01 16:47:54 +0400

    add Forgot Password page

---
+++ src/components/ForgetPasswordSectionTwo.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import GoogleButton from "./GoogleButton";
+
+const ForgetPasswordSectionTwo = () => {
+    return (
+        <div className="flex justify-center mt-36 mb-[148px]">
+            <div className=" w-[320px]">
+                <p className="font-normal text-sm text-[#474B57]">Please enter the email address associated with your account. We'll promptly send you a link to reset your password.</p>
+
+
+                <div className="flex flex-col mt-6">
+                    <label>Email</label>
+                    <input className="w-[320px] h-[45px] rounded-md border-[1px] px-[15px] py-[10px] border-[#E6E7E8]"></input>
+                </div>
+
+
+
+                <button className="w-[318px] h-[44px] py-3 bg-[#0E1422] text-white rounded-[4px] mt-6 text-sm">Send reset link</button>
+            </div>
+
+
+
+
+
+
+
+        </div>
+
+    )
+}
+export default ForgetPasswordSectionTwo;
--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import DiscountBanner from "../components/DiscountBanner";
 import Header from "../components/Header";
+import ForgetPasswordSectionOne from "../components/ForgetPasswordSectionOne";
+import ForgetPasswordSectionTwo from "../components/ForgetPasswordSectionTwo";
+import FooterBlack from "../components/FooterBlack";
 
 const ForgotPasssword =()=>{
     return(
         <div>
             <DiscountBanner/>
             <Header/>
+            <ForgetPasswordSectionOne/>
+            <ForgetPasswordSectionTwo/>
+            <FooterBlack/>
 
         </div>
     )
