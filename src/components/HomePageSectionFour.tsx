7ce2e03eea929a69fc8a6335a5e9290c31c797c2
tamarchkhik <tamarichkhikvadze1@gmail.com>
2024-10-21 17:02:15 +0400

    use productData in HomePageSectionFour

--- src/components/HomePageSectionFour.tsx
+++ src/components/HomePageSectionFour.tsx
@@ -1,71 +1,19 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+import { setProducts } from "../store/app/productSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { ProductsData } from "../components/Data";
+import { RootState } from "../store/store";
 
 const HomePageSectionFour = () => {
-    const products = [
-        {
-            id: 5,
-            img: "/images/image-5.png",
-            title: "Elegant Ebony Sweatshirts",
-            stock: "IN STOCK",
-            price: "$ 35.00",
-        },
-        {
-            id: 6,
-            img: "/images/image-6.png",
-            title: "Sleek and Cozy Black",
-            stock: "IN STOCK",
-            price: "$ 50.00",
-        },
-        {
-            id: 7,
-            img: "/images/image-7.png",
-            title: "Raw Black Tees",
-            stock: "IN STOCK",
-            price: "$ 19.00",
-        },
-        {
-            id: 8,
-            img: "/images/image-8.png",
-            title: "MOCKUP Black",
-            stock: "IN STOCK",
-            price: "$ 30.00",
-        }
-    ]
+    const dispatch = useDispatch()
+    const products = useSelector((state: RootState) => state.product)
+    useEffect(() => {
+        dispatch(setProducts(ProductsData))
+    })
 
-    const items = [
-        {
-            id: 1,
-            img: "/images/image-1.png",
-            title: "Classic Monochrome Tees",
-            stock: "IN STOCK",
-            price: "$ 35.00",
-        },
-        {
-            id: 2,
-            img: "/images/image-2.png",
-            title: "Monochromatic Wardrobe",
-            stock: "IN STOCK",
-            price: "$ 27.00",
-        },
-        {
-            id: 3,
-            img: "/images/image-3.png",
-            title: "Essential Neutrals",
-            stock: "IN STOCK",
-            price: "$ 22.00",
-        },
-        {
-            id: 4,
-            img: "/images/image-4.png",
-            title: "UTRAANET Black",
-            stock: "IN STOCK",
-            price: "$ 43.00",
-        }
-    ]
-
-
-    return (
+   
+     return (
         <div className="mt-[152px] mb-[192px] max-w-[1116px] mx-auto">
             <TabGroup>
                 <TabList className="flex justify-center gap-6">
@@ -81,7 +29,7 @@ const HomePageSectionFour = () => {
                 <TabPanels>
                     <TabPanel className="mt-[80px] grid grid-flow-col gap-10">
 
-                        {products.map((product, index) => (
+                        {products.products.slice(4, 8).map((product:any, index:any) => (
                             <div className="transform transition-transform duration-300
                           hover:scale-105 cursor-pointer">
                                 <a key={index} href={`/${product.id}`}>
@@ -101,16 +49,16 @@ const HomePageSectionFour = () => {
 
                     <TabPanel className="mt-[80px] grid grid-flow-col gap-10">
 
-                        {items.map((item, index) => (
+                        {products.products.slice(0, 4).map((product:any, index:any) => (
                             <div className="transform transition-transform duration-300
                           hover:scale-105 cursor-pointer">
-                                <a key={index} href={`/${item.id}`}>
-                                    <img src={item.img} />
-                                    <h4 className="mt-[25px] font-medium text-sm hover:text-gray-500">{item.title}</h4>
+                                <a key={index} href={`/${product.id}`}>
+                                    <img src={product.img} />
+                                    <h4 className="mt-[25px] font-medium text-sm hover:text-gray-500">{product.title}</h4>
                                     <div className="flex gap-4 mt-[14px]">
-                                        <div className="text-[#0E1422] font-medium text-[12px] border-[#E6E7E8] border-[1px] px-4 py-[2px] rounded-[100px]">{item.stock}</div>
+                                        <div className="text-[#0E1422] font-medium text-[12px] border-[#E6E7E8] border-[1px] px-4 py-[2px] rounded-[100px]">{product.stock}</div>
 
-                                        <span className="text-[#474B57] font-normal text-sm ">{item.price}</span>
+                                        <span className="text-[#474B57] font-normal text-sm ">{product.price}</span>
                                     </div>
                                 </a>
                             </div>
