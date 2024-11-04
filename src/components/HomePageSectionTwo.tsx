4d4d3f66dc8ef77e84b38f2394f0932bf3c1a4fb
tamarchkhik <tamarichkhikvadze1@gmail.com>
2024-11-04 17:39:56 +0400

    add loading

--- src/components/HomePageSectionTwo.tsx
+++ src/components/HomePageSectionTwo.tsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from "react";
 
 const HomePageSectionTwo = () => {
     const [apiData, setApiData] = useState([])
+    const [loading, setLoading] = useState<boolean>(true);
 
 
     const getData = async () => {
+        setLoading(true);
         try {
             const res = await fetch('https://fakestoreapi.com/products?limit=4');
 
@@ -19,6 +21,8 @@ const HomePageSectionTwo = () => {
 
         } catch (error) {
             console.error(error);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -70,24 +74,29 @@ const HomePageSectionTwo = () => {
 
                 </div>
             </div>
-
-            <div className="mt-[80px] grid grid-flow-col gap-10 ">
-                {apiData.map((product: any, index: any) => (
-                    <div className="transform transition-transform duration-300
+            <div>
+                {loading ? (
+                    <h1 className='font-bold text-[#0E1422] text-center text-2xl mt-40'>Loading...</h1>
+                ) : (
+                    <div className="mt-[80px] grid grid-flow-col gap-10 ">
+                        {apiData.map((product: any, index: any) => (
+                            <div className="transform transition-transform duration-300
                     hover:scale-105 cursor-pointer">
-                        <a
-                            key={index} href={`/${product.id}`}>
-                            <img className="w-[237px] h-[312px]"src={product.image} />
-                            <h4 className="mt-[25px] font-medium text-sm hover:text-gray-500">{product.title}</h4>
-                            <div className="flex gap-4 mt-[14px]">
-                                <div className="text-[#0E1422] font-medium text-[12px] border-[#E6E7E8] border-[1px] px-4 py-[2px] rounded-[100px]">{product.stock}</div>
+                                <a
+                                    key={index} href={`/${product.id}`}>
+                                    <img className="w-[237px] h-[312px]" src={product.image} />
+                                    <h4 className="mt-[25px] font-medium text-sm hover:text-gray-500">{product.title}</h4>
+                                    <div className="flex gap-4 mt-[14px]">
+                                        <div className="text-[#0E1422] font-medium text-[12px] border-[#E6E7E8] border-[1px] px-4 py-[2px] rounded-[100px]">{product.stock}</div>
 
-                                <span className="text-[#474B57] font-normal text-sm ">{`$ ${product.price}`}</span>
+                                        <span className="text-[#474B57] font-normal text-sm ">{`$ ${product.price}`}</span>
+                                    </div>
+                                </a>
                             </div>
-                        </a>
-                    </div>
 
-                ))}
+                        ))}
+                    </div>
+                )}
             </div>
 
         </div>
